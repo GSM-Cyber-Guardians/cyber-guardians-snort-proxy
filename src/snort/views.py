33ff832a4007c60ec7f6b8166b0251746a0ce425
Jueuunn7 <wndmsrla1234@gmail.com>
2024-11-27 16:51:41 +0900

    Add get loglist

--- src/snort/views.py
+++ src/snort/views.py
@@ -5,10 +5,15 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import SnortLog
 from .serializers import SnortLogSerializer
 
 
 class SnortLogView(APIView):
+    def get(self, request: Request) -> Response:
+        log = SnortLog.objects.all()
+        serializer = SnortLogSerializer(log, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @atomic
     def post(self, request: Request) -> Response:
