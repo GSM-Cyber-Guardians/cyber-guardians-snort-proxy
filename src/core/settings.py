7e47fb980bff971b110c3205098b427e89b362f8
Jueuunn7 <wndmsrla1234@gmail.com>
2024-11-27 17:36:24 +0900

    add cors

--- src/core/settings.py
+++ src/core/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     # Add-on
     'rest_framework',
     'django_eventstream',
+    'corsheaders',
     # Apps
     'snort',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
