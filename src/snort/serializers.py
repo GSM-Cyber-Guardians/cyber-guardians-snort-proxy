8cc68206043a9ec2af0f1710ecdc8a9023309bc4
Jueuunn7 <wndmsrla1234@gmail.com>
2024-11-28 19:49:14 +0900

    Edit: event_type

--- src/snort/serializers.py
+++ src/snort/serializers.py
@@ -19,7 +19,7 @@ class SnortLogSerializer(serializers.ModelSerializer):
         log = SnortLog.objects.create(**data, type=type)
         send_event(
             channel="snort",
-            event_type="log",
+            event_type="event",
             data={
                 'type': type,
                 'ip': log.ip,
